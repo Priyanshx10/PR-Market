41e1f278994ebb29d004582b6261631685c876eb
Priyansh <Priyanshyadav1012@gmail.com>
2025-01-21 23:48:35 +0530

    Change the theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,13 +56,13 @@ export default function Home() {
                 className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
               >
                 <div className="md:flex-shrink-0 flex justify-center ">
-                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-gray-100 text-blue-600 border-b">
                     {<perk.icons className="h-1/3 w-1/3" />}
                   </div>
                 </div>
 
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
-                  <h3 className="text-base  font-medium text-gray-900">
+                  <h3 className="text-base  font-medium text-blue-600">
                     {perk.name}
                   </h3>
                   <p className="mt-3 text-sm text-muted-foreground">
